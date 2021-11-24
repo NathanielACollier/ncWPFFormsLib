f71f37d8f97b47a30e0e883905200491c167c049
Nathaniel A Collier <drxs3v3n@gmail.com>
2021-11-24 12:20:27 -0600

    Fix test project being able to access the dotnet 6 projects

--- Tests/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("c25f0bb3-f4f3-4c55-bd64-909508e28736")]
